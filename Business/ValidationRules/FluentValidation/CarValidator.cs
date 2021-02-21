6a768496a0ab11dff7a5024227959bd36772407e
Cüneyt Güley <cuneyt.guley@outlook.com>
2021-02-22 02:18:21 +0300

    Odev2 FluentValidation

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.BrandId).NotEmpty();
+            RuleFor(c => c.ColorId).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(200);
+            RuleFor(c => c.ModelYear).GreaterThanOrEqualTo(DateTime.Now.Year - 3);
+            RuleFor(c => c.Description).NotEmpty();
+        }
+    }
+}
